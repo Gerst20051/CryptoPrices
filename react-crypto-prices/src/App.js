1c8d5205d7e42eb2db4bb4db21be3d3e5a99a09a
Andrew Gerst <gerst20051@gmail.com>
2020-12-01 10:38:35 -0800

    handle nulls in crypto data

--- react-crypto-prices/src/App.js
+++ react-crypto-prices/src/App.js
@@ -14,23 +14,23 @@ function transformCryptos(cryptos) {
   const totalMarketCap = cryptos.reduce((total, crypto) => total + crypto.market_cap, 0);
   return cryptos.map(crypto => ({
     ath_date: crypto.ath_date,
-    current_price: `$${parseFloat(crypto.current_price.toFixed(6))}`,
+    current_price: `$${parseFloat((crypto.current_price || 0).toFixed(6))}`,
     days_since_ath: Math.floor(daysBetween(new Date(crypto.ath_date), new Date())),
-    high_24h: crypto.high_24h,
+    high_24h: crypto.high_24h || 0,
     image: crypto.image,
     image_symbol: {
       image: crypto.image,
       symbol: crypto.symbol.toUpperCase(),
     },
-    low_24h: crypto.low_24h,
+    low_24h: crypto.low_24h || 0,
     market_cap: crypto.market_cap,
     market_cap_percentage: `${((crypto.market_cap / totalMarketCap) * 100).toFixed(2)}%`,
     market_cap_rank: crypto.market_cap_rank,
     name: crypto.name,
-    price_change_24h: crypto.price_change_24h,
-    price_change_percentage_24h: `${crypto.price_change_percentage_24h.toFixed(2)}%`,
+    price_change_24h: crypto.price_change_24h || 0,
+    price_change_percentage_24h: `${(crypto.price_change_percentage_24h || 0).toFixed(2)}%`,
     symbol: crypto.symbol.toUpperCase(),
-    total_volume: crypto.total_volume,
+    total_volume: crypto.total_volume || 0,
   }));
 }
 
